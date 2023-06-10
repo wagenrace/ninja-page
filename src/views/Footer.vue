d40d95430bec029aa79fc59bdabaf663c217ee59
wagenrace <nijhof.dns@gmail.com>
2023-06-10 23:00:41 +0200

    add footer

---
+++ src/views/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="bg-company-color-500 text-gray-300 font-bold text-sm w-full h-full text-center py-1">
+    Omas Ninja Service | The Netherlands | Oma is in de kamer met de haai.
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
